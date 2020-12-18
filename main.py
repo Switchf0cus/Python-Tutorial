df00fbddc96cb5cf8c890c0c0e71a7f84e2ae6f3
Pierre Anisimov <pierreanisimov@Pierres-MacBook-Pro.local>
2020-12-18 22:45:25 +0000

    First commit with a few very simple examples of python

---
+++ main.py
@@ -0,0 +1,10 @@
+# print("Shalom")
+# input('What is your name?')
+
+# name = input("What is your name?")
+# print( "Hello " + name)
+
+# x = "Python is"
+# y = " awesomPiee"
+# z = x + y
+# print(z)P
